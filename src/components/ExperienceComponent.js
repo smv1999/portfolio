6fbb22722876a755aebe6fef533673591ad6f35e
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-06-26 22:04:15 +0530

    Updated position in TCS

--- src/components/ExperienceComponent.js
+++ src/components/ExperienceComponent.js
@@ -8,21 +8,27 @@ class Experience extends Component {
   render() {
     const experiences = this.props.details.experiences.map((experience) => {
       return (
-        <div key={experience.id} className="row" style={{padding: "10px"}}>
+        <div key={experience.id} className="row" style={{ padding: "10px" }}>
           <Card className="root" variant="outlined">
             <CardContent>
               <Typography variant="h4" color="textPrimary" gutterBottom>
                 {experience.company}
               </Typography>
-              <Typography variant="h5">
-                {experience.position}
-              </Typography>
-              <Typography className="pos" color="textSecondary">
-                {experience.timeline}
-              </Typography>
-              <Typography variant="body2">
-                {experience.description}
-              </Typography>
+              {experience.positions.map((position) => {
+                return (
+                  <div key={position.id}>
+                    <Typography variant="h5">{position.position}</Typography>
+                    <Typography className="pos" color="textSecondary">
+                      {position.timeline}
+                    </Typography>
+                    <Typography variant="body2">
+                      {position.description}
+                    </Typography>
+                    <br />
+                  </div>
+                );
+                
+              })}
             </CardContent>
           </Card>
         </div>
--- src/shared/details.js
+++ src/shared/details.js
@@ -4,29 +4,28 @@ export const DETAILS = {
   profile_links: {
     email: {
       link: "vaidhyanathan.sm@gmail.com",
-      image: "assets/images/gmail.svg"
+      image: "assets/images/gmail.svg",
     },
     github: {
       link: "https://github.com/smv1999",
-      image: "assets/images/github.svg"
+      image: "assets/images/github.svg",
     },
     linkedin: {
       link: "https://www.linkedin.com/in/vaidhyanathansm/",
-      image : "assets/images/linkedin.svg"
+      image: "assets/images/linkedin.svg",
     },
     medium: {
       link: "https://vaidhyanathansm.medium.com/",
-      image : "assets/images/medium.svg"
+      image: "assets/images/medium.svg",
     },
     hackerrank: {
-      link : "https://www.hackerrank.com/vaidhyanathan_sm?hr_r=1",
-      image : "assets/images/hackerrank.svg"
+      link: "https://www.hackerrank.com/vaidhyanathan_sm?hr_r=1",
+      image: "assets/images/hackerrank.svg",
     },
-    geeksforgeeks:
-      {
-        link : "https://auth.geeksforgeeks.org/user/vaidhyanathansm/profile",
-        image : "assets/images/geeksforgeeks.svg"
-      },
+    geeksforgeeks: {
+      link: "https://auth.geeksforgeeks.org/user/vaidhyanathansm/profile",
+      image: "assets/images/geeksforgeeks.svg",
+    },
   },
   profile_image: "assets/images/profile_image.jpg",
   about_me: {
@@ -73,7 +72,7 @@ export const DETAILS = {
       image: "assets/images/c_lang.svg",
       name: "C",
     },
-    
+
     {
       image: "assets/images/react.svg",
       name: "React",
@@ -118,68 +117,82 @@ export const DETAILS = {
       image: "assets/images/javascript.svg",
       name: "JavaScript",
     },
-        
   ],
-  projects:[
+  projects: [
     {
       id: 1,
       title: "Be Fit - Your Fitness Partner",
       link: "https://play.google.com/store/apps/details?id=com.programmersgateway.sm1999.befit",
-      image: "https://play-lh.googleusercontent.com/YW4d2Z8jZhNE4C6gNFR5kFeV3h8brRp9QAfqgsrxF00TdELiwxRWmyrdEkE7fKhur58=s180-rw",
+      image:
+        "https://play-lh.googleusercontent.com/YW4d2Z8jZhNE4C6gNFR5kFeV3h8brRp9QAfqgsrxF00TdELiwxRWmyrdEkE7fKhur58=s180-rw",
       description: {
-        para1: "The application provides exercises, food and fitness tips for maintaining the health properly.",
-        para2: "The application is made completely user-friendly and has an amazing user-experience and has a dark mode in its very first version itself!"
-      }
+        para1:
+          "The application provides exercises, food and fitness tips for maintaining the health properly.",
+        para2:
+          "The application is made completely user-friendly and has an amazing user-experience and has a dark mode in its very first version itself!",
+      },
     },
     {
       id: 2,
       title: "Sketch Tab - Your Art Playground",
       link: "https://play.google.com/store/apps/details?id=com.programmersgateway.sm1999.sketchtab",
-      image: "https://play-lh.googleusercontent.com/arjwweu2ecu2V3OWozZUYYm67xWir-8njlyBtmayu33nhmZTFuBZ_E5ZkJe2-loNhg=s180-rw",
+      image:
+        "https://play-lh.googleusercontent.com/arjwweu2ecu2V3OWozZUYYm67xWir-8njlyBtmayu33nhmZTFuBZ_E5ZkJe2-loNhg=s180-rw",
       description: {
-        para1: "Kick off the artist in you and get your hands to work on something cool. Play with the tools in the art gallery and enjoy drawing. It is completely offline.",
-        para2: "The app is completely user-friendly and has a good user interface."
-      }
+        para1:
+          "Kick off the artist in you and get your hands to work on something cool. Play with the tools in the art gallery and enjoy drawing. It is completely offline.",
+        para2:
+          "The app is completely user-friendly and has a good user interface.",
+      },
     },
     {
       id: 3,
       title: "Covid-19 Sahayak",
       link: "https://drive.google.com/file/d/189Xq2Gb28e5wd8Mja5F4-znbFo06U7K3/view",
-      image: "https://raw.githubusercontent.com/smv1999/smv1999.github.io/master/coronavirus.png",
+      image:
+        "https://raw.githubusercontent.com/smv1999/smv1999.github.io/master/coronavirus.png",
       description: {
         para1: "Provides live updates state-wise and at the world-level",
-        para2: "Funds and helpline, Essentials and Maps are some unique features. The app has an Amazing UI/UX and comes with Dark mode in its very first version!"
-      }
+        para2:
+          "Funds and helpline, Essentials and Maps are some unique features. The app has an Amazing UI/UX and comes with Dark mode in its very first version!",
+      },
     },
     {
       id: 4,
       title: "Weather Heute - Know your Weather",
       link: "https://play.google.com/store/apps/details?id=com.programmersgateway.sm1999.weatherheute",
-      image: "https://play-lh.googleusercontent.com/J0ReGAfHAH3bm3hVJz5VH1N2aAbv-1AFv-JpbFlwRi_KhelJvTYI575uMtv74hGODA=s180-rw",
+      image:
+        "https://play-lh.googleusercontent.com/J0ReGAfHAH3bm3hVJz5VH1N2aAbv-1AFv-JpbFlwRi_KhelJvTYI575uMtv74hGODA=s180-rw",
       description: {
-        para1: "Climate change is sometimes misunderstood as being about changes in the weather. In reality, it is about changes in our very way of life.",
-        para2: "Get the weather updates according to your current location.Also, get weather updates of major cities of the world."
-      }
+        para1:
+          "Climate change is sometimes misunderstood as being about changes in the weather. In reality, it is about changes in our very way of life.",
+        para2:
+          "Get the weather updates according to your current location.Also, get weather updates of major cities of the world.",
+      },
     },
     {
       id: 5,
       title: "Dev Portal - Community Portal for Developers",
       link: "https://play.google.com/store/apps/details?id=com.programmersgateway.sm1999.dev_portal",
-      image: "https://play-lh.googleusercontent.com/4D3fo8kk8XYPV2kcVZKZdeL07hTxAFFuRKkAWRm7FLOT6TtNrCINKGB5uS7t_whbalZK=s180-rw",
+      image:
+        "https://play-lh.googleusercontent.com/4D3fo8kk8XYPV2kcVZKZdeL07hTxAFFuRKkAWRm7FLOT6TtNrCINKGB5uS7t_whbalZK=s180-rw",
       description: {
         para1: "Dev Portal is a Community Portal for Developers.",
-        para2: "It is an initiative to bring the developers across the world to connect with each other and it creates an environment for learning and growing together."
-      }
+        para2:
+          "It is an initiative to bring the developers across the world to connect with each other and it creates an environment for learning and growing together.",
+      },
     },
     {
       id: 6,
       title: "Math Your Way - Fall in Love with Math",
       link: "https://play.google.com/store/apps/details?id=com.programmersgateway.sm1999.mathyourway",
-      image: "https://play-lh.googleusercontent.com/8pHsVCV8O9fOKFSGAsp5vKY5GFBTJNKPx5k_NyyVhAmITaClfnkN2VpHIH1HGIW5OA=s180-rw",
+      image:
+        "https://play-lh.googleusercontent.com/8pHsVCV8O9fOKFSGAsp5vKY5GFBTJNKPx5k_NyyVhAmITaClfnkN2VpHIH1HGIW5OA=s180-rw",
       description: {
-        para1: "There are extraordinary features such as Advanced and Basic Calculators and Math puzzles and Games that will make children to love Mathematics.",
-        para2: "With amazing UI and design, you will Fall in love with Math!"
-      }
+        para1:
+          "There are extraordinary features such as Advanced and Basic Calculators and Math puzzles and Games that will make children to love Mathematics.",
+        para2: "With amazing UI and design, you will Fall in love with Math!",
+      },
     },
     {
       id: 7,
@@ -187,9 +200,11 @@ export const DETAILS = {
       link: "https://github.com/smv1999/Xpense",
       image: "https://smv1999.github.io/expenses.png",
       description: {
-        para1: "This is a Expense management system that can be used by housewives at home.",
-        para2: "It manages the daily expenses and creates a bill.! It is very user-friendly and works efficiently."
-      }
+        para1:
+          "This is a Expense management system that can be used by housewives at home.",
+        para2:
+          "It manages the daily expenses and creates a bill.! It is very user-friendly and works efficiently.",
+      },
     },
     {
       id: 8,
@@ -197,9 +212,11 @@ export const DETAILS = {
       link: "https://github.com/smv1999/custom-header-files-cpp",
       image: "assets/images/cplusplus.svg",
       description: {
-        para1: "This is a Expense management system that can be used by housewives at home.",
-        para2: "It manages the daily expenses and creates a bill.! It is very user-friendly and works efficiently."
-      }
+        para1:
+          "This is a Expense management system that can be used by housewives at home.",
+        para2:
+          "It manages the daily expenses and creates a bill.! It is very user-friendly and works efficiently.",
+      },
     },
     {
       id: 9,
@@ -207,44 +224,75 @@ export const DETAILS = {
       link: "https://github.com/smv1999/Password-Generator-and-Manager",
       image: "assets/images/password.svg",
       description: {
-        para1: "Custom Utility Header Files are created from scratch in C++ for performing various operations on Data Structures.",
-        para2: "Additional functionalities are added for performing various operations on Arrays, Linked Lists and Trees."
-      }
-    }
+        para1:
+          "Custom Utility Header Files are created from scratch in C++ for performing various operations on Data Structures.",
+        para2:
+          "Additional functionalities are added for performing various operations on Arrays, Linked Lists and Trees.",
+      },
+    },
   ],
-  experiences:[
+  experiences: [
     {
-      id:1,
-      position: "Project Intern",
-      type: "Internship",
+      id: 1,
       company: "Tata Consultancy Services",
-      description: "Worked on an Internal Project",
-      timeline: "Mar 2021 - May 2021"
+      positions: [
+        {
+          id: 1,
+          position: "Project Intern",
+          type: "Internship",
+          description: "Worked in a Project named Smart Browser Console.",
+          timeline: "Mar 2021 - May 2021",
+        },
+        {
+          id: 2,
+          position: "Systems Engineer",
+          type: "Full Time",
+          description: "Working on TCS MasterCraft DataPlus",
+          timeline: "May 2021 - Present",
+        },
+      ],
     },
     {
-      id:2,
-      position: "Technical Lead",
-      type: "Internship",
+      id: 2,
       company: "GirlScript Foundation",
-      description: "Part Time - Worked in the field of Web Development and maintenance of the website",
-      timeline: "May 2020 - Sep 2020"
+      positions: [
+        {
+          id: 1,
+          position: "Technical Lead",
+          type: "Internship",
+          description:
+            "Part Time - Worked in the field of Web Development and maintenance of the website",
+          timeline: "May 2020 - Sep 2020",
+        },
+      ],
     },
     {
-      id:3,
-      position: "Assistant Developer",
-      type: "Internship",
+      id: 3,
       company: "SocialFriend Stressfree LLP",
-      description: "Worked with Flask framework for creating their Web Platform. Built a robust Android App for creating their Mobile Platform. The application both in Mobile and Web version is a platform developed for school students which connects them with mentors and teachers",
-      timeline: "Mar 2020 - June 2020"
+      positions: [
+        {
+          id: 1,
+          position: "Assistant Developer",
+          type: "Internship",
+          description:
+            "Worked with Flask framework for creating their Web Platform. Built a robust Android App for creating their Mobile Platform. The application both in Mobile and Web version is a platform developed for school students which connects them with mentors and teachers",
+          timeline: "Mar 2020 - June 2020",
+        },
+      ],
     },
     {
-      id:4,
-      position: "Software Developer",
-      type: "Internship",
+      id: 4,
       company: "Toshiba Machine (Chennai) Pvt Ltd",
-      description: "Worked with Qt (C++) framework for creation of HMI for their manufacturing sector to monitor the working of their machines.",
-      timeline: "Jun 2020 - Jul 2019"
+      positions: [
+        {
+          id: 1,
+          position: "Software Developer",
+          type: "Internship",
+          description:
+            "Worked with Qt (C++) framework for creation of HMI for their manufacturing sector to monitor the working of their machines.",
+          timeline: "Jun 2020 - Jul 2019",
+        },
+      ],
     },
-        
-  ]
+  ],
 };
